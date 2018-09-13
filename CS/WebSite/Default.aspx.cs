9505259e3de750eba879171cac874d3d544bfb95
Roman Balaban <25555034+onebalaban@users.noreply.github.com>
2018-09-13 18:20:50 +0300

    Refactoring

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -1,14 +1,7 @@
 using System;
-using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 
-public partial class _Default : System.Web.UI.Page {
+public partial class _Default : Page {
 	protected void Page_Load(object sender, EventArgs e) {
 
 	}
